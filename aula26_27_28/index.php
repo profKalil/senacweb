c14cb4c8fa50f39927d1a1ba16e0ead6ad75bed9
profKalil <84177112+profKalil@users.noreply.github.com>
2023-09-14 18:12:42 -0300

    Add files via upload

---
+++ aula26_27_28/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <title>Restaurante Zabot</title>
+</head>
+<body>
+    <?php 
+        $teste = 'Bem-vindo ao restaurante Zabot!';
+
+        echo '<h1>'.$teste.'</h1>';
+        print $teste;
+    ?>
+
+    <script src="/js/bootstrap.min.js"> </script>
+
+</body>
+</html>
