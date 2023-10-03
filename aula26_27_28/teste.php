f258482a7fc4ec3979d29869deb4a44fc51492b3
profKalil <84177112+profKalil@users.noreply.github.com>
2023-10-03 12:54:57 -0300

    Add files via upload

---
+++ aula26_27_28/teste.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Teste Bootstrap com PHP</title>
+<link rel="stylesheet" href="css/bootstrap.min.css"></link>
+</head>
+
+<body>
+
+<h1>funcionou sem php</h1>
+
+<?php
+
+echo "<h1>funcionou com php</h1>";
+
+?>
+
+</body>
+</html>
